a687146f701db720f4c22469eda149eccd728b74
Anirudha Kulkarni <kulkarnianirudha8@gmail.com>
2023-02-14 22:44:42 +0530

    multiclass metric

--- AUCM_exp/SupContrast/scripts/aucm.sh
+++ AUCM_exp/SupContrast/scripts/aucm.sh
@@ -1,37 +1,23 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
-python3 main_aucm.py --batch_size 256 \
+# table 1
+## ResNet20
+### AUCM
+export CUDA_VISIBLE_DEVICES=7
+python main_aucm.py --batch_size 256 \
   --learning_rate 0.8 \
   --cosine \
   --imratio 0.01 \
   --dataset cifar10 \
   --model resnet20 &
 
-export CUDA_VISIBLE_DEVICES=1
-python3 main_aucm.py --batch_size 256 \
-  --learning_rate 0.8 \
-  --cosine \
-  --imratio 0.01 \
-  --dataset cifar10 \
-  --model densenet121 &
-
-export CUDA_VISIBLE_DEVICES=2
-python3 main_aucm.py --batch_size 256 \
+export CUDA_VISIBLE_DEVICES=7
+python main_aucm.py --batch_size 256 \
   --learning_rate 0.8 \
   --cosine \
   --imratio 0.01 \
   --dataset cifar100 \
   --model resnet20 &
 
-export CUDA_VISIBLE_DEVICES=3
-python3 main_aucm.py --batch_size 256 \
-  --learning_rate 0.8 \
-  --cosine \
-  --imratio 0.01 \
-  --dataset cifar100 \
-  --model densenet121 &
-
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=7
 python3 main_aucm.py --batch_size 128 \
   --learning_rate 0.8 \
   --cosine \
@@ -39,7 +25,46 @@ python3 main_aucm.py --batch_size 128 \
   --dataset c2 \
   --model resnet20 &
 
-export CUDA_VISIBLE_DEVICES=5
+
+export CUDA_VISIBLE_DEVICES=7
+python3 main_aucm.py --batch_size 128 \
+  --learning_rate 0.8 \
+  --cosine \
+  --imratio 0.01 \
+  --dataset stl10 \
+  --model resnet20 &
+
+
+
+### nll
+export CUDA_VISIBLE_DEVICES=7
+python main_nll.py --batch_size 256 \
+  --learning_rate 0.8 \
+  --cosine \
+  --imratio 0.01 \
+  --dataset cifar10 \
+  --model resnet20 &
+
+
+## DenseNet121
+### AUCM
+export CUDA_VISIBLE_DEVICES=6
+python main_aucm.py --batch_size 256 \
+  --learning_rate 0.8 \
+  --cosine \
+  --imratio 0.01 \
+  --dataset cifar10 \
+  --model densenet121 &
+
+export CUDA_VISIBLE_DEVICES=6
+python main_aucm.py --batch_size 256 \
+  --learning_rate 0.8 \
+  --cosine \
+  --imratio 0.01 \
+  --dataset cifar100 \
+  --model densenet121 &
+
+export CUDA_VISIBLE_DEVICES=6
 python3 main_aucm.py --batch_size 128 \
   --learning_rate 0.8 \
   --cosine \
@@ -53,49 +78,31 @@ python3 main_aucm.py --batch_size 128 \
   --cosine \
   --imratio 0.01 \
   --dataset stl10 \
-  --model resnet20 &
-
-export CUDA_VISIBLE_DEVICES=7
-python3 main_aucm.py --batch_size 128 \
-  --learning_rate 0.8 \
-  --cosine \
-  --imratio 0.01 \
-  --dataset stl10 \
   --model densenet121 &
 
-export CUDA_VISIBLE_DEVICES=0
-python3 main_aucm.py --batch_size 256 \
+
+
+
+# Table 8
+## ResNet20
+### AUCM
+export CUDA_VISIBLE_DEVICES=5
+python main_aucm.py --batch_size 256 \
   --learning_rate 0.8 \
   --cosine \
   --imratio 0.1 \
   --dataset cifar10 \
   --model resnet20 &
 
-export CUDA_VISIBLE_DEVICES=1
-python3 main_aucm.py --batch_size 256 \
-  --learning_rate 0.8 \
-  --cosine \
-  --imratio 0.1 \
-  --dataset cifar10 \
-  --model densenet121 &
-
-export CUDA_VISIBLE_DEVICES=2
-python3 main_aucm.py --batch_size 256 \
+export CUDA_VISIBLE_DEVICES=5
+python main_aucm.py --batch_size 256 \
   --learning_rate 0.8 \
   --cosine \
   --imratio 0.1 \
   --dataset cifar100 \
   --model resnet20 &
 
-export CUDA_VISIBLE_DEVICES=3
-python3 main_aucm.py --batch_size 256 \
-  --learning_rate 0.8 \
-  --cosine \
-  --imratio 0.1 \
-  --dataset cifar100 \
-  --model densenet121 &
-
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=5
 python3 main_aucm.py --batch_size 128 \
   --learning_rate 0.8 \
   --cosine \
@@ -108,22 +115,44 @@ python3 main_aucm.py --batch_size 128 \
   --learning_rate 0.8 \
   --cosine \
   --imratio 0.1 \
-  --dataset c2 \
-  --model densenet121 &
-
-export CUDA_VISIBLE_DEVICES=6
-python3 main_aucm.py --batch_size 128 \
-  --learning_rate 0.8 \
-  --cosine \
-  --imratio 0.1 \
   --dataset stl10 \
   --model resnet20 &
 
-export CUDA_VISIBLE_DEVICES=7
+## DenseNet121
+### AUCM
+export CUDA_VISIBLE_DEVICES=4
+python main_aucm.py --batch_size 256 \
+    --learning_rate 0.8 \
+    --cosine \
+    --imratio 0.1 \
+    --dataset cifar10 \
+    --model densenet121 &
+
+export CUDA_VISIBLE_DEVICES=4
+python main_aucm.py --batch_size 256 \
+    --learning_rate 0.8 \
+    --cosine \
+    --imratio 0.1 \
+    --dataset cifar100 \
+    --model densenet121 &
+
+export CUDA_VISIBLE_DEVICES=4
 python3 main_aucm.py --batch_size 128 \
-  --learning_rate 0.8 \
-  --cosine \
-  --imratio 0.1 \
-  --dataset stl10 \
-  --model densenet121 &
+    --learning_rate 0.8 \
+    --cosine \
+    --imratio 0.1 \
+    --dataset c2 \
+    --model densenet121 &
+
+export CUDA_VISIBLE_DEVICES=4
+python3 main_aucm.py --batch_size 128 \
+    --learning_rate 0.8 \
+    --cosine \
+    --imratio 0.1 \
+    --dataset stl10 \
+    --model densenet121
+
+
+
+
 
